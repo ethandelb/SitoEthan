5def82324a3ddb9d19df48c2c245dbe2f9e9414f
ethandelb <ethandelbiaggiobelli@gmail.com>
2026-04-22 07:41:03 +0200

    nuove foto

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,5 @@
 <footer class="footer">
     <p>&copy; <?php echo date("Y"); ?> - <?php echo SITE_NAME; ?> </p>
-    <p><?php echo SITE_EMAIL;?></p>
 </footer>
 <script src="assets/js/main.js"></script>
 
